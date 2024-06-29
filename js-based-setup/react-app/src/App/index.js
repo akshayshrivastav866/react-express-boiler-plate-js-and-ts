f815edae22f26240514c9db1304062d409e68351
Akshay Shrivastav <akshayshrivastav866@gmail.com>
2024-06-29 20:37:41 +0530

    Added SCSS support for react

---
+++ js-based-setup/react-app/src/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './index.scss'
+
+function App() {
+  return (
+    <div className='app'>
+      <header className='app__header'>Hurray!! React App is Ready</header>
+    </div>
+  )
+}
+
+export default App
