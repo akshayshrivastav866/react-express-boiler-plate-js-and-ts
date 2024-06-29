227bb4fb82229cae2711d4de6099bbf60fa96682
Akshay Shrivastav <akshayshrivastav866@gmail.com>
2024-06-29 20:19:46 +0530

    TS Based Folder - Added Folder Structure + Updated imports, using scss instead of css

---
+++ ts-based-setup/react-app/src/App/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './index.scss';
+
+function App() {
+	return (
+		<div className="app">
+			<header className="app__header">
+				"Hurray!! React + TypeScript App is Ready"
+			</header>
+		</div>
+	);
+}
+
+export default App;
