93860e66c2da87b364a9b8c2e4bfd922ba6a2e9a
Akshay Shrivastav <akshayshrivastav866@gmail.com>
2024-06-29 20:37:02 +0530

    JS Based - Added dotenv support for express + updated ports

--- js-based-setup/express-app/index.js
+++ js-based-setup/express-app/index.js
@@ -1,8 +1,21 @@
 const express = require( 'express' );
+const dotenv = require( 'dotenv' );
 const app = express();
 
-app.get( '/', ( req, res ) => {
-    res.send( 'Hurray!! Express Server is Live' );
-} );
+const port = process.env.PORT || 3000;
 
-app.listen( 3000 );
+dotenv.config();
+
+const payload = {
+	status: 200,
+	message: 'Hurray!! Express Server is Live'
+};
+
+app.get('/', (req, res) => {
+	res.send(payload);
+});
+
+app.listen(port, () => {
+	console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
